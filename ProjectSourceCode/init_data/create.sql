58eb4cb936e69b4e6a1401bb3985c83818482b37
MarcusDeLuca <made1570@colorado.edu>
2025-04-03 17:20:39 -0600

    Building sql tables and filling the courses table

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -14,6 +14,7 @@ CREATE TABLE courses (
   course_id NUMERIC PRIMARY KEY,
   course_name VARCHAR(100) NOT NULL,
   credit_hours NUMERIC NOT NULL
+  specific_major VARCHAR(100) NULL
 );
 
 DROP TABLE IF EXISTS student_courses;
@@ -23,30 +24,16 @@ CREATE TABLE student_courses (
 );
   
 DROP TABLE IF EXISTS prerequisites;
-CREATE TABLE IF NOT EXISTS prerequisites (
-  course_id INTEGER NOT NULL REFERENCES courses (course_id),
-  prerequisite_id INTEGER NOT NULL REFERENCES courses (course_id)
+
+CREATE TABLE prerequisites (
+  course_id NUMERIC NOT NULL,
+  prerequisite_id NUMERIC NOT NULL,
+  PRIMARY KEY (course_id, prerequisite_id),
+  FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE,
+  FOREIGN KEY (prerequisite_id) REFERENCES courses(course_id) ON DELETE CASCADE
 );
 
 
 
 
-CREATE OR REPLACE VIEW course_prerequisite_count AS
-  SELECT
-    c.course_id,
-    COUNT(p.prerequisite_id) AS num_prerequisites
-  FROM
-    courses AS c
-    LEFT JOIN prerequisites AS p ON c.course_id = p.course_id
-  GROUP BY c.course_id;
 
-CREATE OR REPLACE VIEW student_prerequisite_count AS
-  SELECT
-    c.course_id,
-    sc.student_id,
-    COUNT(*) AS num_prerequisites_satisfied
-  FROM
-    courses AS c
-    LEFT JOIN prerequisites AS p ON c.course_id = p.course_id
-    LEFT JOIN student_courses AS sc ON p.prerequisite_id = sc.course_id
-  GROUP BY c.course_id, sc.student_id;
